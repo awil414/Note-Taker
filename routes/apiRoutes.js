cf5798ffebd2e75d015001b98f0f437239be2932
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-18 10:54:35 -0500

    made changes to GET api/notes and started POST api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,29 @@
 // Setting up dependencies
+const uuid = require('uuid');
 const fs = require('fs');
 const db = require('../db/db.json');
 // const uuid = require('uuid');
 
 // Routing
 module.exports = (app) => {
+
     // GET api/notes reads the db.json file and returns saved notes as JSON
     app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../db/db.json'));
+        res.json(db);
+    });
+
+
+    // POST api/notes receives new note, adds to db.json file, and returns new note to user
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFile('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        // Creating body for note
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            // Creating uuid
+            id: uuid(),
+        };
     })
 }
