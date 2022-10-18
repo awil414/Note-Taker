c108e1eebeb15f46b2d405768f879be04adad289
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-18 11:42:47 -0500

    made changes to server.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,10 @@
 const uuid = require('uuid');
 const fs = require('fs');
 const db = require('../db/db.json');
+const fb = require('express').Router();
 // const uuid = require('uuid');
 
-// Routing
-module.exports = (app) => {
+
 
     // GET api/notes reads the db.json file and returns saved notes as JSON
     app.get('/api/notes', (req, res) => {
@@ -30,4 +30,6 @@ module.exports = (app) => {
         fs.writeFile('db/db.json', JSON.stringify(db));
         res.json(db);
     });
-};
+
+
+module.exports = fb;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // Setting up dependencies
 const express = require('express');
-// const fs = require('fs');
 const path = require('path');
+const api = require('./routes/index.js');
 
-// Setting up Express App
 const app = express();
 
 // Setting up port
@@ -12,13 +11,20 @@ const PORT = process.env.PORT || 3001;
 // Middleware -- setting up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', api);
 // Setting up static path
 app.use(express.static('public'));
 
-// GET basic routes
-require('./routes/apiRoutes')(app);
-require('/routes/htmlRoutes')(app);
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 // Setting up listener
 app.listen(PORT, () =>
-    console.log(`App is listening at PORT:${PORT}`));
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
