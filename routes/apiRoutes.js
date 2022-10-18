54d4b7a18f6ecd5768e0c426a6d6002c6017460a
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-10-18 10:33:41 -0500

    made changes to GET function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,11 @@
 // Setting up dependencies
 const fs = require('fs');
 const db = require('../db/db.json');
-const uuid = require('uuid');
+// const uuid = require('uuid');
 
 // Routing
 module.exports = (app) => {
     // GET api/notes reads the db.json file and returns saved notes as JSON
-    app.get('/api/notes', (req, res) => res.json(db));
     app.get('/api/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../db/db.json'));
     })
